f903f969a6788ce813a0e1286b577279551add61
Preeti Basyal <preetibasyal412@gmail.com>
2023-03-17 21:36:20 +0545

    counting character frequency in the string/usr/bin/python3.9

--- String/Ex1.py
+++ String/Ex1.py
@@ -10,3 +10,15 @@ for letter in giver_str:
     else:
         dir1[letter]+=1
 print(dir1)
+
+
+#  Write a Python program to get a string made of the first 2 and last 2 characters of a given string. If the string length is less than 2, return the empty string instead.
+# Sample String : 'w3resource'      Expected Result : 'w3ce'
+# Sample String : 'w3'              Expected Result : 'w3w3'
+# Sample String : ' w'              Expected Result : Empty String
+
+input_str = input("Please enter the string.")
+if len(input_str)<2:
+    print("")
+else:
+    print(input_str[:2] + input_str[-2:])
